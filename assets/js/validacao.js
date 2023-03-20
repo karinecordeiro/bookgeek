33324c754db5c832ef1d91731f0076b9e79d08ae
Karine C <karine.cordeiro1998@hotmail.com>
2023-03-20 16:00:27 -0300

    adicionando seção de contato

---
+++ assets/js/validacao.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById('formulario');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.input__erro');
+
+function inserirErro(index){
+    campos[index].style.borderBottom = '2px solid #b92424';
+    spans[index].style.display = 'block';
+}
+function removerErro(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nomeValidado(){
+    if(campos[0].value.length < 3){
+        inserirErro(0);
+    } else{ 
+        removerErro(0);
+    }
+}
+function mensagemValidado(){
+    if(campos[1].value.length < 3 || campos[1].value.length >= 300){
+        inserirErro(1);
+    } else{ 
+        removerErro(1);
+    }
+}
+function enviar(){
+    
+}
